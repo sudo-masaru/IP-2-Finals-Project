fc1f025c6278c4bd75e44f61bf8f91eddd7e60df
stanly-sudo <alys.git24@gmail.com>
2025-05-07 21:22:28 +0800

    designed table

--- js/demo/datatables-demo.js
+++ js/demo/datatables-demo.js
@@ -1,4 +1,10 @@
 // Call the dataTables jQuery plugin
 $(document).ready(function() {
-  $('#dataTable').DataTable();
+    $('#dataTable').DataTable({
+        bFilter: false,       // Disable the search bar
+        bLengthChange: false, // Disable the count selector
+        info: false,
+        ordering: false,
+        paging: false
+    });
 });
